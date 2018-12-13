16e3547d356d4b501841b9499b5575980d6c5734
yuduobing <1510557673@qq.com>
2018-12-13 15:11:24 +0800

    mpvueswiper修改

--- src/pages/logs/index.vue
+++ src/pages/logs/index.vue
@@ -1,43 +1,56 @@
 <template>
   <div>
-    <ul class="container log-list">
-      <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
-        <card :text="(index + 1) + ' . ' + log"></card>
-      </li>
-    </ul>
+    <swiper class="swiper"  previous-margin="30px"  next-margin="30px" interval="5000" duration="1000">
+      <block v-for="(item, index) in books" :index="index" :key="key">
+        <swiper-item>
+          <div class="im1">
+          <image :src="item.tupian" class="slide-image" mode="aspectFill"  />
+          {{item.text}}</div>
+        </swiper-item>
+      </block>
+    </swiper>
+
+
   </div>
 </template>
- 
+
 <script>
-import { formatTime } from '@/utils/index'
-import card from '@/components/card'
 
 export default {
-  components: {
-    card
-  },
 
   data () {
     return {
-      logs: []
+      logs: [],
+      books: [
+        {
+          text: '平凡的世界',
+          url: 'http://www.baidu.com',
+          tupian: 'https://img3.doubanio.com/view/subject/l/public/s27449344.jpg'
+        },
+        {
+          text: '活着',
+          url: 'http://www.tencent.com',
+          tupian: 'https://img3.doubanio.com/view/subject/l/public/s23836852.jpg'
+        },
+        {
+          text: '解忧杂货店',
+          url: 'http://www.baidu.com',
+          tupian: 'https://img3.doubanio.com/view/subject/l/public/s27264181.jpg'
+        }]
     }
   },
+  methods: {
 
-  created () {
-    const logs = (wx.getStorageSync('logs') || [])
-    this.logs = logs.map(log => formatTime(new Date(log)))
   }
 }
 </script>
 
-<style>
-.log-list {
-  display: flex;
-  flex-direction: column;
-  padding: 40rpx;
-}
-
-.log-item {
-  margin: 10rpx;
-}
+<style scoped>
+  .swiper {
+    height: 600px !important;
+  }
+  image {
+    height: 500px;
+    width: 100%;
+  }
 </style>
